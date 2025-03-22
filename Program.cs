c40036561f249c30a990a304506169ac6cd389e6
rcocks <richard.cocks@gmail.com>
2025-03-22 00:00:01 +0000

    Benchmarks

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using BenchmarkDotNet.Running;
+using PasswordGen;
+
+/*
+var x = new PasswordGenerator()
+{
+    n = 12
+};
+Console.WriteLine($"PasswordChatGpt: {x.PasswordChatGpt()}");
+Console.WriteLine($"PasswordChatGptStringBuilder: {x.PasswordChatGptStringBuilder()}");
+Console.WriteLine($"PasswordUsingCharBufferCharLookupSpan: {x.PasswordUsingCharBufferCharLookupSpan()}");
+Console.WriteLine($"PasswordUsingCharBufferCharLookupReducedSetPregen: {x.PasswordUsingCharBufferCharLookupReducedSetPregen()}");
+*/
+
+BenchmarkRunner.Run<PasswordGenerator>();
